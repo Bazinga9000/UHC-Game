17ae0dd3d029b3f38e34861fb1142dca4f327d30
Bazinga9000 <bazonkidonk@gmail.com>
2020-12-17 14:34:45 -0500

    plugin skeleton

---
+++ src/main/java/xyz/baz9k/UHCGame/TestListener.java
@@ -0,0 +1,7 @@
+package xyz.baz9k.UHCGame;
+
+import org.bukkit.event.Listener;
+
+public class TestListener implements Listener {
+
+}
--- src/main/java/xyz/baz9k/UHCGame/UHCGame.java
+++ src/main/java/xyz/baz9k/UHCGame/UHCGame.java
@@ -1,7 +1,14 @@
 package xyz.baz9k.UHCGame;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
 
 public class UHCGame extends JavaPlugin {
-
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new TestListener(), this);
+    }
 }
