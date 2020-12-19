f6267ca80a009b9508c4b6c5c5c86573e49515ca
endrdragon <daendrdragon@gmail.com>
2020-12-19 13:36:32 -0500

    uhcPlugin => plugin (for consistency)

--- src/main/java/xyz/baz9k/UHCGame/UHCManager.java
+++ src/main/java/xyz/baz9k/UHCGame/UHCManager.java
@@ -7,48 +7,48 @@ import org.bukkit.potion.PotionEffect;
 import java.util.HashMap;
 
 public class UHCManager {
-    private UHCGame uhcPlugin;
+    private UHCGame plugin;
 
     private boolean isUHCStarted = false;
     private final HashMap<Player, String> previousDisplayNames;
     private final UHCTeamManager teamManager = new UHCTeamManager();
-
+    
     private UHCHUDManager hudManager;
 
     public UHCManager(UHCGame plugin) {
-        uhcPlugin = plugin;
+        this.plugin = plugin;
         previousDisplayNames = new HashMap<>();
         hudManager = new UHCHUDManager(plugin);
-        uhcPlugin.getServer().getPluginManager().registerEvents(hudManager, uhcPlugin);
+        plugin.getServer().getPluginManager().registerEvents(hudManager, plugin);
     }
 
 
     public void startUHC() {
         isUHCStarted = true;
-
-        for (Player p : uhcPlugin.getServer().getOnlinePlayers()) {
+        
+        for (Player p : plugin.getServer().getOnlinePlayers()) {
             //archive previous display name
             previousDisplayNames.put(p, p.getDisplayName());
-
+            
             //fully heal, adequately saturate, remove XP
             p.setHealth(20.0f);
             p.setSaturation(5.0f);
             p.setExp(0.0f);
-
+            
             //clear all potion effects
             for (PotionEffect effect : p.getActivePotionEffects()) {
                 p.removePotionEffect(effect.getType());
             }
-
+            
             if (teamManager.isPlayerSpectator(p)) {
                 p.setGameMode(GameMode.SPECTATOR);
             }
 
         }
-
+        
         hudManager.start();
     }
-
+    
     public void endUHC() {
         isUHCStarted = false;
 
