074edc7dd5fd03ccc918e28ef03bff8cf4861964
endrdragon <daendrdragon@gmail.com>
2020-12-22 23:54:09 -0500

    use hex over hex strings because might as well

--- src/main/java/xyz/baz9k/UHCGame/util/TeamColors.java
+++ src/main/java/xyz/baz9k/UHCGame/util/TeamColors.java
@@ -6,19 +6,22 @@ import java.awt.*;
 
 public class TeamColors {
     private static final int numTeamColors = 64;
-    private static final String[] teamColorCodes = {"c04040", "4040c0", "40c040", "c0c040", "c06b40", "6b40c0", "40c0c0", "6bc040",
-            "c09640", "9640c0", "4096c0", "40c096", "96c040", "c040c0", "406bc0", "40c06b", "c04096", "c06bc0",
-            "6bc06b", "c0966b", "c0406b", "c06b96", "6b96c0", "96c06b", "c06b6b", "964040", "6b6bc0", "6bc0c0",
-            "c0c06b", "966b40", "966bc0", "6bc096", "969640", "406b40", "404096", "40966b", "6b9640", "406b6b",
-            "96406b", "406b96", "409640", "40406b", "6b4040", "964096", "409696", "6b406b", "6b6b40", "6b4096",
-            "c09696", "404040", "966b6b", "96c0c0", "c0c096", "6b6b6b", "6b9696", "c096c0", "96c096", "969696",
-            "6b6b96", "6b966b", "9696c0", "c0c0c0", "966b96", "96966b"};
+    private static final int[] teamColorCodes = {
+        0xc04040, 0x4040c0, 0x40c040, 0xc0c040, 0xc06b40, 0x6b40c0, 0x40c0c0, 0x6bc040,
+        0xc09640, 0x9640c0, 0x4096c0, 0x40c096, 0x96c040, 0xc040c0, 0x406bc0, 0x40c06b,
+        0xc04096, 0xc06bc0, 0x6bc06b, 0xc0966b, 0xc0406b, 0xc06b96, 0x6b96c0, 0x96c06b,
+        0xc06b6b, 0x964040, 0x6b6bc0, 0x6bc0c0, 0xc0c06b, 0x966b40, 0x966bc0, 0x6bc096,
+        0x969640, 0x406b40, 0x404096, 0x40966b, 0x6b9640, 0x406b6b, 0x96406b, 0x406b96,
+        0x409640, 0x40406b, 0x6b4040, 0x964096, 0x409696, 0x6b406b, 0x6b6b40, 0x6b4096,
+        0xc09696, 0x404040, 0x966b6b, 0x96c0c0, 0xc0c096, 0x6b6b6b, 0x6b9696, 0xc096c0,
+        0x96c096, 0x969696, 0x6b6b96, 0x6b966b, 0x9696c0, 0xc0c0c0, 0x966b96, 0x96966b
+    };
 
     private static Color[] teamColors = new Color[numTeamColors];
 
     static {
         for (int i = 0; i < numTeamColors; i++) {
-            teamColors[i] = Color.decode("#" + teamColorCodes[i].toUpperCase());
+            teamColors[i] = new Color(teamColorCodes[i]);
         }
     }
 
@@ -53,5 +56,4 @@ public class TeamColors {
     public static String getTeamPrefixWithSpace(int teamIndex) {
         return getTeamPrefix(teamIndex) + " " + ChatColor.RESET;
     }
-
 }
