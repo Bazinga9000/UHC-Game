64701bb6e4cd5db95a90d4af8ded5a652552b60d
endrdragon <daendrdragon@gmail.com>
2020-12-24 00:15:38 -0500

    init config manager

---
+++ src/main/java/xyz/baz9k/UHCGame/ConfigManager.java
@@ -0,0 +1,13 @@
+package xyz.baz9k.UHCGame;
+
+public class ConfigManager {
+    private final UHCGame plugin;
+    private final GameManager gameManager;
+
+    public ConfigManager(UHCGame plugin, GameManager gameManager) {
+        this.plugin = plugin;
+        this.gameManager = gameManager;
+    }
+
+    
+}
