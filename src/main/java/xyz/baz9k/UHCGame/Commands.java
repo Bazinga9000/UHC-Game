f1fcf599b02b0f2502ed428ffb6e7a525eb89989
endrdragon <daendrdragon@gmail.com>
2020-12-24 16:49:46 -0500

    clearer /uhc start, /uhc end errors

--- src/main/java/xyz/baz9k/UHCGame/Commands.java
+++ src/main/java/xyz/baz9k/UHCGame/Commands.java
@@ -53,11 +53,12 @@ public class Commands {
         .executes(
             (sender, args) -> {
                 try {
-                    Bukkit.broadcastMessage("UHC attempting start");
+                    Bukkit.broadcastMessage("[DEBUG] UHC attempting start");
                     plugin.getGameManager().startUHC();
-                    Bukkit.broadcastMessage("UHC started");
+                    Bukkit.broadcastMessage("[DEBUG] UHC started");
                 } catch (IllegalStateException e) {
-                    CommandAPI.fail("UHC has already started!");
+                    CommandAPI.fail(e.getMessage());
+                    throw e;
                 }
             }
         ).register();
@@ -72,11 +73,12 @@ public class Commands {
         .executes(
             (sender, args) -> {
                 try {
-                    Bukkit.broadcastMessage("UHC attempting end");
+                    Bukkit.broadcastMessage("[DEBUG] UHC attempting end");
                     plugin.getGameManager().endUHC();
-                    Bukkit.broadcastMessage("UHC ended");
+                    Bukkit.broadcastMessage("[DEBUG] UHC ended");
                 } catch (IllegalStateException e) {
-                    CommandAPI.fail("UHC has not started!");
+                    CommandAPI.fail(e.getMessage());
+                    throw e;
                 }
             }
         ).register();
