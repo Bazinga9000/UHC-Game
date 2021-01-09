e121247e4b7d0125aa4acfc0e8c5333b5213bcb1
endrdragon <daendrdragon@gmail.com>
2021-01-09 16:21:00 -0500

    use hypot over sqrt & pow

--- src/main/java/xyz/baz9k/UHCGame/util/Utils.java
+++ src/main/java/xyz/baz9k/UHCGame/util/Utils.java
@@ -29,6 +29,6 @@ public class Utils {
     }
 
     public static double euclideanDistance(double x1, double x2, double y1, double y2) {
-        return Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
+        return Math.hypot(x1 - x2, y1 - y2);
     }
 }
