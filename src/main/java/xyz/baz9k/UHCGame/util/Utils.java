7d6d6ebf1081f0b00971e95797dd5f11e3123b71
endrdragon <daendrdragon@gmail.com>
2021-01-15 19:43:55 -0500

    javadocs Utils.java

--- src/main/java/xyz/baz9k/UHCGame/util/Utils.java
+++ src/main/java/xyz/baz9k/UHCGame/util/Utils.java
@@ -3,31 +3,81 @@ package xyz.baz9k.UHCGame.util;
 import java.time.Duration;
 
 public class Utils {
+    /**
+     * Get a time string of the provided number of seconds.
+     * Minutes and seconds are provided by default, and hours are provided if the amount of time provided exceeds an hour.
+     * @param s
+     * @return the time string
+     * 
+     * @see #getLongTimeString(long)
+     */
     public static String getTimeString(long s) {
         if (s < 3600) return String.format("%02d:%02d", s / 60, (s % 60));
         return String.format("%d:%02d:%02d", s / 3600, (s % 3600) / 60, (s % 60));
     }
 
+    /**
+     * Get a time string of the provided {@link Duration}'s duration.
+     * Minutes and seconds are provided by default, and hours are provided if the amount of time provided exceeds an hour.
+     * @param d
+     * @return the time string
+     * 
+     * @see #getLongTimeString(Duration)
+     */
     public static String getTimeString(Duration d) {
         return getTimeString(d.toSeconds());
     }
 
+    /**
+     * Get a long time string of the provided number of seconds.
+     * Hours, minutes, and seconds are all provided in the string.
+     * @param s
+     * @return
+     */
     public static String getLongTimeString(long s) {
         return String.format("%d:%02d:%02d", s / 3600, (s % 3600) / 60, (s % 60));
     }
 
+    /**
+     * Get a long time string of the provided {@link Duration}'s duration.
+     * Hours, minutes, and seconds are all provided in the string.
+     * @param d
+     * @return
+     */
     public static String getLongTimeString(Duration d) {
         return getLongTimeString(d.toSeconds());
     }
 
+    /**
+     * Take the modulo where 0 <= x < y.
+     * Java's operation % takes the remainder and is negative if x is negative.
+     * @param x
+     * @param y
+     * @return result of the modulo
+     */
     public static int mod(int x, int y) {
         return ((x % y) + y) % y;
     }
 
+    /**
+     * Take the modulo where 0 <= x < y.
+     * Java's operation % takes the remainder and is negative if x is negative.
+     * @param x
+     * @param y
+     * @return result of the modulo
+     */
     public static double mod(double x, double y) {
         return ((x % y) + y) % y;
     }
 
+    /**
+     * Takes the euclidean distance between points (x1, y1) and (x2, y2).
+     * @param x1
+     * @param x2
+     * @param y1
+     * @param y2
+     * @return distance calculation
+     */
     public static double euclideanDistance(double x1, double x2, double y1, double y2) {
         return Math.hypot(x1 - x2, y1 - y2);
     }
