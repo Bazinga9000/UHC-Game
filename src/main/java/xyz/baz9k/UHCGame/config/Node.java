8ab7277a61e36c3159c480e0265139317a13153a
endrdragon <daendrdragon@gmail.com>
2021-05-16 02:15:23 -0400

    impl updateItemStack for all except option types

--- src/main/java/xyz/baz9k/UHCGame/config/Node.java
+++ src/main/java/xyz/baz9k/UHCGame/config/Node.java
@@ -1,21 +1,33 @@
 package xyz.baz9k.UHCGame.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import xyz.baz9k.UHCGame.UHCGame;
 
 public abstract class Node {
     protected BranchNode parent;
     protected ItemStack itemStack;
+    protected List<String> itemDesc;
     protected static UHCGame plugin;
     protected static FileConfiguration cfg;
     protected int parentSlot;
 
     public Node(BranchNode parent, int parentSlot, ItemStack item) {
         this.parent = parent;
+
         this.itemStack = item;
+
+        // Creates a copy of the lore. 
+        // This is necessary because ValuedNodes use the original desc as a template, so they must use this to update their descs.
+        ItemMeta m = item.getItemMeta();
+        this.itemDesc = m.hasLore() ? m.getLore() : new ArrayList<>();
+
         this.parentSlot = parentSlot;
         if (parent != null) {
             parent.setChild(parentSlot, this);
--- src/main/java/xyz/baz9k/UHCGame/config/ValuedNode.java
+++ src/main/java/xyz/baz9k/UHCGame/config/ValuedNode.java
@@ -1,14 +1,30 @@
 package xyz.baz9k.UHCGame.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class ValuedNode extends Node {
     protected final ValuedNodeType type;
     protected final String id;
 
+    /**
+     * @param parent
+     * @param slot
+     * @param item The item
+     * <p>
+     * If format strings are included in the item's description (%s, %.1f, etc.), those will be substituted with the config value.
+     * @param type
+     * @param id
+     */
     public ValuedNode(BranchNode parent, int slot, ItemStack item, ValuedNodeType type, String id) {
         super(parent, slot, item);
         this.type = type;
@@ -24,7 +40,6 @@ public class ValuedNode extends Node {
     public void click(@NotNull Player p) {
         Object val;
         switch (type) {
-            // TODO impl prompts for int, dbl, string  
             case INTEGER:
                 val = cfg.getInt(id);
                 break;
@@ -52,26 +67,42 @@ public class ValuedNode extends Node {
      */
     public void updateItemStack() {
         ItemMeta m = itemStack.getItemMeta();
+        m.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        List<String> newDesc = new ArrayList<>(itemDesc);
 
         // perform updates based on data type
         switch (type) {
             case INTEGER:
-                int configValue = cfg.getInt(id);
-                this.itemStack.setAmount(configValue);
-                break;
             case DOUBLE:
-                break;
             case STRING:
-                break;
+                // use the initially provided desc for format
+                Object val = cfg.get(id);
+                for (int i = 0; i < itemDesc.size(); i++) {
+                    String line = itemDesc.get(i);
+                    newDesc.set(i, String.format(line, val));
+                }
             case BOOLEAN:
+                // keeps the description untouched, adds Status: ACTIVE/INACTIVE below it
+                String status = ChatColor.WHITE + "Status: ";
+                
+                if (cfg.getBoolean(id)) {
+                    status += ChatColor.GREEN + "ACTIVE";
+                    m.addEnchant(Enchantment.SILK_TOUCH, 1, true);
+                } else {
+                    status += ChatColor.RED + "INACTIVE";
+                    m.removeEnchant(Enchantment.SILK_TOUCH);
+                }
+                newDesc.add("");
+                newDesc.add(status);
                 break;
-
-            // OPTION impl in OptionValuedNode
-
+                
+                // OPTION impl in OptionValuedNode
+                
             default:
                 throw new UnsupportedOperationException("Type not supported");
         }
-
+            
+        m.setLore(newDesc);
         itemStack.setItemMeta(m);
         // since updating the item does not update it in the inventory, parent has to
         parent.updateSlot(parentSlot);
