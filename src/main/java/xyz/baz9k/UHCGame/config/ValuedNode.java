a7b0abee8a6cafdc6088a102b9169608d6a4e627
endrdragon <daendrdragon@gmail.com>
2021-05-16 01:02:47 -0400

    updateItemStack stuffs

--- src/main/java/xyz/baz9k/UHCGame/config/ValuedNode.java
+++ src/main/java/xyz/baz9k/UHCGame/config/ValuedNode.java
@@ -2,6 +2,7 @@ package xyz.baz9k.UHCGame.config;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import xyz.baz9k.UHCGame.UHCGame;
 
@@ -25,19 +26,32 @@ public class ValuedNode extends Node {
 
     }
 
+    /**
+     * Update the item stack based on the current config value for the node
+     */
     public void updateItemStack() {
-        switch(type) {
+        ItemMeta m = itemStack.getItemMeta();
+
+        // perform updates based on data type
+        switch (type) {
             case INTEGER:
                 int configValue = plugin.getConfig().getInt(id);
                 this.itemStack.setAmount(configValue);
                 break;
             case DOUBLE:
+                break;
             case STRING:
+                break;
             case BOOLEAN:
+                break;
             case OPTION:
+                break;
             default:
+                throw new UnsupportedOperationException("Type not supported");
         }
 
+        itemStack.setItemMeta(m);
+        // since updating the item does not update it in the inventory, parent has to
         parent.updateSlot(parentSlot);
     }
 }
