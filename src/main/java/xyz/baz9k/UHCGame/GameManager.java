21c62070ec339c6ee7f6d53d3fc9f1755fa9c6d7
endrdragon <daendrdragon@gmail.com>
2021-05-18 01:12:55 -0400

    oops

--- src/main/java/xyz/baz9k/UHCGame/GameManager.java
+++ src/main/java/xyz/baz9k/UHCGame/GameManager.java
@@ -102,7 +102,7 @@ public class GameManager implements Listener {
                 }
             }
             if (!worldsRegened) {
-                throw new IllegalStateException("UHC worlds have not been regenerated. Run /reseed to regenerate.");
+                throw new IllegalStateException("UHC worlds have not been regenerated. Run /uhc reseed to regenerate.");
             }
         } else {
             Debug.broadcastDebug("Skipping starting requirements");
