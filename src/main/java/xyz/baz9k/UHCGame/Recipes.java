892693a256656549e15fd7c7536627bc6ca2540e
endrdragon <daendrdragon@gmail.com>
2022-01-03 21:00:14 -0500

    recipe class rehaul

--- src/main/java/xyz/baz9k/UHCGame/Recipes.java
+++ src/main/java/xyz/baz9k/UHCGame/Recipes.java
@@ -6,10 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Keyed;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
@@ -20,27 +22,63 @@ import org.jetbrains.annotations.NotNull;
 
 public final class Recipes {
     private final UHCGamePlugin plugin;
-    private final Set<NamespacedKey> registered = new HashSet<>();
+    private final Map<NamespacedKey, RecipeProperties> registered = new HashMap<>();
     public Recipes(UHCGamePlugin plugin) {
         this.plugin = plugin;
     }
 
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    private @interface DeclaredRecipe {
+        EnableWhen enable();
+    }
+
+    private enum EnableWhen {
+        ALWAYS (c -> true),
+        PLAYER_DROPS_HEAD (c -> c.playerDropsIndex() == 2),
+        PROX_TRACK (ConfigValues::proxTrack);
+
+        private Predicate<ConfigValues> pred;
+        private EnableWhen(Predicate<ConfigValues> pred) {
+            this.pred = pred;
+        }
+
+        public boolean test(ConfigValues cfg) { return pred.test(cfg); }
+    }
+
+    public record RecipeProperties(Recipe r, EnableWhen pred) {
+        public NamespacedKey key() {
+            if (r instanceof Keyed kr) {
+                return kr.getKey();
+            }
+            return null;
+        }
+
+        public boolean test(ConfigValues cfg) {
+            return pred.test(cfg);
+        }
+    }
+
     public void registerAll() {
-        // register each @Command method
+        // register each @DeclaredRecipe method
+        Class<DeclaredRecipe> recipeCls = DeclaredRecipe.class;
         try {
             for (Method m : Recipes.class.getDeclaredMethods()) {
-                if (!m.isAnnotationPresent(DeclaredRecipe.class)) continue;
-                Bukkit.addRecipe((Recipe) m.invoke(this));
+                if (m.isAnnotationPresent(recipeCls)) {
+                    Recipe r = (Recipe) m.invoke(this);
+                    EnableWhen enableWhen = m.getAnnotation(recipeCls).enable();
+                    RecipeProperties props = new RecipeProperties(r, enableWhen);
+                    NamespacedKey key = props.key();
+
+                    if (key != null) {
+                        registered.put(key, props);
+                        Bukkit.addRecipe(r);
+                    }
+                }
             }
         } catch (IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
-    }
-    public void unregisterAll() {
-        for (NamespacedKey key : registered) {
-            Bukkit.removeRecipe(key);
-        }
-        registered.clear();
     }
 
     /**
@@ -48,32 +86,43 @@ public final class Recipes {
      * @param p Player to register recipes for
      */
     public void discoverFor(@NotNull Player p) {
-        p.discoverRecipes(registered);
+        p.discoverRecipes(registered.keySet());
+
+        Iterable<Recipe> it = Bukkit::recipeIterator;
+        for (Recipe r : it) {
+            if (r instanceof Keyed kr) {
+                p.discoverRecipe(kr.getKey());
+            }
+        }
     }
 
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target(ElementType.METHOD)
-    private @interface DeclaredRecipe { }
+    public boolean isRecipeEnabled(Recipe r) {
+        if (r instanceof Keyed kr) {
+            NamespacedKey key = kr.getKey();
+            if (registered.containsKey(key)) {
+                RecipeProperties props = registered.get(key);
+                var cfg = plugin.configValues();
+                return props.test(cfg);
+            };
+        }
+        return true;
+    }
 
     private @NotNull NamespacedKey key(String s) {
         NamespacedKey nkey = new NamespacedKey(plugin, s);
-        registered.add(nkey);
         return nkey;
     }
 
     /* RECIPES */
-    @DeclaredRecipe
+    @DeclaredRecipe(enable = EnableWhen.ALWAYS)
     private Recipe godApple() {
-        ShapedRecipe recipe = new ShapedRecipe(key("godapple"), new ItemStack(Material.ENCHANTED_GOLDEN_APPLE));
-
-        recipe.shape(
-            "GGG",
-            "GAG",
-            "GGG"
-        )
-        .setIngredient('G', Material.GOLD_BLOCK)
-        .setIngredient('A', Material.APPLE);
-
-        return recipe;
+        return new ShapedRecipe(key("godapple"), new ItemStack(Material.ENCHANTED_GOLDEN_APPLE))
+            .shape(
+                "GGG",
+                "GAG",
+                "GGG"
+            )
+            .setIngredient('G', Material.GOLD_BLOCK)
+            .setIngredient('A', Material.APPLE);
     }
 }
